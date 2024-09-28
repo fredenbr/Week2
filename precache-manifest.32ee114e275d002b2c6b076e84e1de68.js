2a7112a5e4b66fb5095d101f0b080be8759b190f
Bridget Hopkins <bridgethopkins@Mac-mini.local>
2024-09-28 12:37:52 -0400

    Updates

---
+++ precache-manifest.32ee114e275d002b2c6b076e84e1de68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d60406d47aca1c958a9ad8e3c00d061",
+    "url": "/Week2/index.html"
+  },
+  {
+    "revision": "6db6ba27240031ccb907",
+    "url": "/Week2/static/css/main.cfb30404.chunk.css"
+  },
+  {
+    "revision": "ed260f1f86ea4bc6afaf",
+    "url": "/Week2/static/js/2.e4e3aed9.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Week2/static/js/2.e4e3aed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfac136dae66cca0916",
+    "url": "/Week2/static/js/3.9f79fc85.chunk.js"
+  },
+  {
+    "revision": "6db6ba27240031ccb907",
+    "url": "/Week2/static/js/main.ad6836c7.chunk.js"
+  },
+  {
+    "revision": "b497f0db98c6720f97cc",
+    "url": "/Week2/static/js/runtime-main.709a963c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Week2/static/media/logo.06e73328.svg"
+  }
+]);
